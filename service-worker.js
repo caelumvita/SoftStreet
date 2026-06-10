d35056149d17b560141d82463d7c377ea2bc1885
caelumvita <caelumvitas@gmail.com>
2026-06-10 15:36:08 +0300

    Update service worker cache version to v100

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dawnline-cache-v54";
+const CACHE_NAME = "dawnline-cache-v100";
 
 const FILES = [
   "./",
@@ -12,17 +12,14 @@ const FILES = [
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
+    caches.keys().then(keys => {
       return Promise.all(
-        keys.map((key) => {
+        keys.map(key => {
           if (key !== CACHE_NAME) return caches.delete(key);
         })
       );
@@ -34,8 +31,6 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
